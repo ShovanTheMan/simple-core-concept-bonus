7c9d94ff65ee93ce4128c90d7ccd36df5f8ae649
ShovanTheMan <shovan2192@gmail.com>
2023-11-01 16:27:01 +0600

    quiz added

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function compare(a, b) {
+
+    if (a == b) {
+   
+    return true;
+   
+    } else {
+   
+    return false;
+   
+    }
+   
+   }
+   
+   const result = compare(15, 25);
+   
+   console.log(result);
+
+//    console.log("123"+ 123)
+//    answer is --> 123123
